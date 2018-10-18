ff62f4eb5e719b3720c537269c62815a3d11bf60
Sanne Grinovero <sanne@hibernate.org>
2018-10-18 18:31:37 +0100

    Remove unused classes

--- src/test/java/org/jboss/jandex/test/TestMe.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.jandex.test;
-
-/**
- * @author Jason T. Greene
- */
-public @interface TestMe {
-    public String value() default "Foo";
-    public String test() default "haha";
-}

144a6cb20681e3a361547f0e998abb8b02761792
Ladislav Thon <lthon@redhat.com>
2021-08-31 16:36:12 +0200

    Move from javadoc annotations to actual Java annotations in the mojo
    Additionally, the integration test now verifies actual content
    of the index, not just existence (and non-emptiness) of the file.

--- maven-plugin/src/it/smoketest/verify.groovy
+++ maven-plugin/src/it/smoketest/verify.groovy
@@ -1,3 +1,8 @@
+import org.jboss.jandex.IndexReader
+
 def jandexFile = new File(basedir, 'target/classes/META-INF/jandex.idx')
-assert jandexFile.exists() : "File does not exist: ${jandexFile}"
-assert jandexFile.length() > 0 : "File is empty: ${jandexFile}"
+assert jandexFile.exists() : "File ${jandexFile} does not exist"
+assert jandexFile.length() > 0 : "File ${jandexFile} is empty"
+
+def index = new IndexReader(jandexFile.newInputStream()).read()
+assert index.getKnownClasses().size() == 1 : "Index ${jandexFile} doesn't contain exactly 1 class"
--- maven-plugin/src/main/java/org/jboss/jandex/maven/JandexGoal.java
+++ maven-plugin/src/main/java/org/jboss/jandex/maven/JandexGoal.java
@@ -9,10 +9,12 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import org.apache.maven.plugin.Mojo;
+import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.codehaus.plexus.util.DirectoryScanner;
 import org.codehaus.plexus.util.IOUtil;
 import org.jboss.jandex.ClassInfo;
@@ -22,76 +24,64 @@ import org.jboss.jandex.Indexer;
 
 /**
  * Generate a Jandex index for classes compiled as part of the current project.
- *
- * @goal jandex
- * @phase process-classes
- * @threadSafe
- * @author jdcasey
  */
-public class JandexGoal implements Mojo {
+@Mojo(name = "jandex", defaultPhase = LifecyclePhase.PROCESS_CLASSES, threadSafe = true)
+public class JandexGoal extends AbstractMojo {
 
     /**
      * By default, process the classes compiled for the project. If you need to process other sets of classes, such as
      * test classes, see the "fileSets" parameter.
-     *
-     * @parameter default-value="${project.build.outputDirectory}"
-     * @readonly
      */
+    @Parameter(defaultValue = "${project.build.outputDirectory}", readonly = true)
     private File classesDir;
 
     /**
      * Process the classes found in these file-sets, after considering the specified includes and excludes, if any. The
-     * format is: <br/>
+     * format is:
      *
      * <pre>
      * <code>
      * &lt;fileSets&gt;
      *   &lt;fileSet&gt;
      *     &lt;directory&gt;path-or-expression&lt;/directory&gt;
-     *     &ltincludes&gt;
-     *       &lt;include&gt;some/thing/*.good&lt;/include&gt;
      *     &lt;includes&gt;
+     *       &lt;include&gt;some/thing/*.good&lt;/include&gt;
+     *     &lt;/includes&gt;
      *     &lt;excludes&gt;
      *       &lt;exclude&gt;some/thing/*.bad&lt;/exclude&gt;
-     *     &lt;/excludes&lt;
+     *     &lt;/excludes&gt;
      *   &lt;/fileSet&gt;
      * &lt;/fileSets&gt;
      * </code>
      * </pre>
      *
-     * <br>
      * <em>NOTE: Standard globbing expressions are supported in includes/excludes.</em>
-     *
-     * @parameter
      */
+    @Parameter
     private List<FileSet> fileSets;
 
     /**
      * If true, construct an implied file-set using the target/classes directory, and process the classes there.
-     *
-     * @parameter default-value="true"
      */
+    @Parameter(defaultValue = "true")
     private boolean processDefaultFileSet = true;
 
     /**
      * Print verbose output (debug output without needing to enable -X for the whole build)
-     *
-     * @parameter default-value="false"
      */
+    @Parameter(defaultValue = "false")
     private boolean verbose = false;
 
     /**
      * The name of the index file. Default's to 'jandex.idx'
-     *
-     * @parameter default-value="jandex.idx"
      */
+    @Parameter(defaultValue = "jandex.idx")
     private String indexName = "jandex.idx";
 
     /**
      * Skip execution if set.
-     *
-     * @parameter default-value="false" property="jandex.skip"
      */
+    @Parameter(property = "jandex.skip", defaultValue = "false")
     private boolean skip = true;
 
     private Log log;
@@ -201,37 +191,5 @@ public class JandexGoal implements Mojo {
 
     private boolean doVerbose() {
         return verbose || getLog().isDebugEnabled();
-    }
-
-    public boolean isVerbose() {
-        return verbose;
-    }
-
-    public void setVerbose(final boolean verbose) {
-        this.verbose = verbose;
-    }
-
-    public boolean isSkip() {
-        return skip;
-    }
-
-    public void setSkip(final boolean skip) {
-        this.skip = skip;
-    }
-
-    public void setLog(final Log log) {
-        this.log = log;
-    }
-
-    public Log getLog() {
-        return log;
-    }
-
-    public String getIndexName() {
-        return indexName;
-    }
-
-    public void setIndexName(String indexName) {
-        this.indexName = indexName;
     }
 }

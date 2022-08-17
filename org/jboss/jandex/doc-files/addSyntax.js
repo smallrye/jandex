60fbc572dad82ea9074e66114bbbf50abb5a10f0
Ladislav Thon <lthon@redhat.com>
2022-08-17 15:49:38 +0200

    remove javadoc

--- org/jboss/jandex/doc-files/addSyntax.js
+++
@@ -1,5 +0,0 @@
-var ref=document.createElement("link")
-ref.setAttribute("rel", "stylesheet")
-ref.setAttribute("type", "text/css")
-ref.setAttribute("href", "doc-files/syntax.css")
-document.head.appendChild(ref);

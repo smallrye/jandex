75f38c399a443a327653910bf4f04a9b38f1e6de
Ladislav Thon <lthon@redhat.com>
2022-09-23 17:52:24 +0200

    Improve how the `test-data` module is built using JDK 17
    Previously, the `test-data` module was always built using a second JDK
    downloaded during build using a Maven plugin. This is hostile to downstream
    rebuilds in an air-gapped environment and makes the release process
    more complex and fragile for no good reason.
    
    With this commit, a second JDK is only downloaded if the "current" JDK
    is older than 17. If the "current" JDK is 17+, it is used to build all
    modules, including `test-data`. We still want to make sure that the core
    Jandex library, as well as the Maven plugin, produce Java 8 bytecode.
    For that reason, this commit adds an automated test to both modules which
    verifies that the class file version of some class is `52` (Java 8).
    
    Also, this commit changes the JDK version used for release to 17, to
    eat our own dog food.

---
+++ core/src/test/java/org/jboss/jandex/test/BytecodeVersionTest.java
@@ -0,0 +1,49 @@
+package org.jboss.jandex.test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Release builds (as well as downstream rebuilds) of Jandex are supposed to be built using JDK 17,
+ * so that the {@code test-data} module doesn't have to download a second JDK during the build process,
+ * but we want to make sure those builds still produce Java 8 bytecode.
+ */
+public class BytecodeVersionTest {
+    @Test
+    public void verifyJava8() throws IOException {
+        try (InputStream in = BytecodeVersionTest.class.getResourceAsStream("/org/jboss/jandex/Main.class")) {
+            if (in == null) {
+                fail("Could not find org.jboss.jandex.Main");
+            }
+
+            DataInputStream data = new DataInputStream(new BufferedInputStream(in));
+            verifyMagic(data);
+            verifyVersion(data);
+        }
+    }
+
+    private void verifyMagic(DataInputStream stream) throws IOException {
+        byte[] buf = new byte[4];
+
+        stream.readFully(buf);
+        if (buf[0] != (byte) 0xCA || buf[1] != (byte) 0xFE || buf[2] != (byte) 0xBA || buf[3] != (byte) 0xBE) {
+            fail("Invalid magic value: " + Arrays.toString(buf));
+        }
+    }
+
+    private void verifyVersion(DataInputStream stream) throws IOException {
+        int minor = stream.readUnsignedShort();
+        int major = stream.readUnsignedShort();
+
+        if (major != 52) { // Java 8
+            fail("Unexpected class file format version: " + major + "." + minor + ", Jandex must be Java 8 bytecode");
+        }
+    }
+}

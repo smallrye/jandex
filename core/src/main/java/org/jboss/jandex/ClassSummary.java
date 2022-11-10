4fa2ec48ee358343acd8d1934c78042109908f1e
Ladislav Thon <lthon@redhat.com>
2022-11-10 16:14:14 +0100

    Add javadoc for ClassSummary

--- core/src/main/java/org/jboss/jandex/ClassSummary.java
+++ core/src/main/java/org/jboss/jandex/ClassSummary.java
@@ -1,7 +1,16 @@
 package org.jboss.jandex;
 
+import java.io.InputStream;
 import java.util.Set;
 
+/**
+ * Summary of a just-indexed class, returned by {@link Indexer#indexWithSummary(InputStream)}.
+ * Contains overview information for reporting progress in batch indexers, as well as
+ * some structural information that can safely be exposed to callers during indexing.
+ * <p>
+ * This class must not expose information that could possibly change during post-processing,
+ * when completing the index.
+ */
 public final class ClassSummary {
     private final DotName name;
     private final DotName superclassName;
@@ -13,18 +22,30 @@ public final class ClassSummary {
         this.annotations = annotations;
     }
 
+    /**
+     * Returns the name of this class.
+     */
     public DotName name() {
         return name;
     }
 
+    /**
+     * Returns the name of this class's superclass.
+     */
     public DotName superclassName() {
         return superclassName;
     }
 
+    /**
+     * Returns the names of annotations present in this class.
+     */
     public Set<DotName> annotations() {
         return annotations;
     }
 
+    /**
+     * Returns the number of types of annotations present in this class.
+     */
     public int annotationsCount() {
         return annotations.size();
     }

92c60c92395eccfb1d06c76bda2434210be1b9ea
Martin Kouba <mkouba@redhat.com>
2023-06-06 15:54:33 +0200

    Add MethodInfo#isDefault() and MethodInfo#isBridge()

--- core/src/test/java/org/jboss/jandex/test/ModifiersTestCase.java
+++ core/src/test/java/org/jboss/jandex/test/ModifiersTestCase.java
@@ -1,11 +1,17 @@
 package org.jboss.jandex.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import org.jboss.jandex.ClassInfo;
+import org.jboss.jandex.DotName;
 import org.jboss.jandex.FieldInfo;
 import org.jboss.jandex.Index;
 import org.jboss.jandex.MethodInfo;
@@ -58,6 +64,35 @@ public class ModifiersTestCase {
     }
 
     @Test
+    public void testMethodIsDefault() throws Exception {
+        ClassInfo clazz = BasicTestCase.getClassInfo(MyInterface.class);
+        assertTrue(clazz.method("defaultVal").isDefault());
+        assertFalse(clazz.method("val").isDefault());
+        assertFalse(clazz.method("age").isDefault());
+        MethodInfo constructor = BasicTestCase.getClassInfo(DummyTopLevel.class).method("<init>");
+        assertFalse(constructor.isDefault());
+    }
+
+    @Test
+    public void testMethodIsBridge() throws Exception {
+        ClassInfo clazz = BasicTestCase.getClassInfo(FooWithBridge.class);
+        List<MethodInfo> acceptMethods = clazz.methods().stream().filter(m -> m.name().equals("accept"))
+                .collect(Collectors.toList());
+        assertEquals(2, acceptMethods.size());
+        for (MethodInfo m : acceptMethods) {
+            if (m.parameterType(0).name().equals(DotName.OBJECT_NAME)) {
+                // accept(Object)
+                assertTrue(m.isBridge());
+                assertTrue(m.isSynthetic());
+            } else {
+                // accept(String)
+                assertFalse(m.isBridge());
+                assertFalse(m.isSynthetic());
+            }
+        }
+    }
+
+    @Test
     public void testFieldIsSynthetic() throws Exception {
         final byte[] syntheticClass = new ByteBuddy().subclass(Object.class)
                 .visit(new ModifierAdjustment().withTypeModifiers(SyntheticState.SYNTHETIC))
@@ -74,6 +109,28 @@ public class ModifiersTestCase {
         }
         assertNotNull(ping);
         assertTrue(ping.isSynthetic());
+    }
+
+    interface MyInterface {
+
+        static int age() {
+            return 1;
+        }
+
+        boolean val() throws Exception;
+
+        default boolean defaultVal() {
+            return true;
+        }
+
+    }
+
+    public static class FooWithBridge implements Consumer<String> {
+
+        @Override
+        public void accept(String val) {
+        }
+
     }
 
 }
